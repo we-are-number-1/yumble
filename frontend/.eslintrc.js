49f9973dce4e52a34909c8758edb01ad78abe130
Alex Verkerk <44216323+Alxjlv@users.noreply.github.com>
2021-03-11 22:03:56 +1300

    Added eslint to both frontend and backend (#23)
    * Added eslint to both frontend and backend
    
    * Fixed all current linting issues + added some defaults

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true,
+  },
+  'extends': [
+    'plugin:react/recommended',
+    'google',
+  ],
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true,
+    },
+    'ecmaVersion': 12,
+    'sourceType': 'module',
+  },
+  'plugins': [
+    'react',
+  ],
+  'rules': {
+    // suppress errors for missing 'import React' in files
+    'react/react-in-jsx-scope': 'off',
+  },
+};
