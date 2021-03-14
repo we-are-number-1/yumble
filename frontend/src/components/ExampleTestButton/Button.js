77dd08802deb20228a029311d16ca80a202f61bc
David Xiao <50573329+David-Xia0@users.noreply.github.com>
2021-03-14 16:20:13 +1300

    [BUG] Modify Eslint rules for react/propTypes (#55)
    * test
    
    * fixed es Lint issues

--- frontend/src/components/ExampleTestButton/Button.js
+++ frontend/src/components/ExampleTestButton/Button.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
+/**
+ *
+ * @param {*} text
+ * @return {*}
+ */
 export default function Button({text}) {
-    return (
-        <div>
-            <button>{text}</button>
-        </div>
-    );
+  return (
+    <div>
+      <button>{text}</button>
+    </div>
+  );
 }
