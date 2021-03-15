e57b78edf37cdc65e5008eaf9cbc294a551ed968
Tianrens <tshe695@aucklanduni.ac.nz>
2021-03-15 22:14:46 +1300

    Join room socket backend

--- backend/src/sockets/joinRoom.js
+++ backend/src/sockets/joinRoom.js
@@ -1,13 +1,16 @@
+import games from '../domain/Games';
 /**
  * Handles event when user joins a room. Adds to corresponding roomid.
  * @param {*} socket
  * @param {*} io
  */
 export function joinRoom(socket, io) {
-  socket.on('join_room', ({roomid, username}) => {
-    // Join room
-    socket.join(roomid);
-    console.log(username);
-    io.to(roomid).emit('new_user', {username});
+  socket.on('join_room', ({gameid, name}) => {
+    const game = games.getGame(gameid);
+    if (!game.session.host) {
+      game.session.host = socket;
+    }
+    game.session.addUser(socket, name);
+    io.to(roomid).emit('new_user', game.session.users);
   });
 }
