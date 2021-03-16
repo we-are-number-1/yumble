57db76c034c98b73dbaff57740217f7071207dd7
Alex Verkerk <44216323+Alxjlv@users.noreply.github.com>
2021-03-16 22:55:01 +1300

    Added dummy backend endpoint

--- backend/src/routes/sessions.js
+++ backend/src/routes/sessions.js
@@ -1,6 +1,17 @@
 import express from 'express';
+import Restaurant from '../mongo/models/Restaurant';
 const router = express.Router();
 
 // TODO: Handlers...
+// GET:
+router.get('/testCard', async (req, res) => {
+  const testCard = new Restaurant({
+    name: 'Lonestar',
+    location: 'Owen G Glen Building',
+    cuisine: 'European',
+    price: '5',
+  });
+  res.status(200).json(testCard);
+});
 
 export default router;
