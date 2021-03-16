4034b399627b1186dc29496baf5d893381d24561
jemimahmartinez <martinezjemimah@gmail.com>
2021-03-17 08:17:53 +1300

    Created a temporary constant for the map's src to be passed as props

--- frontend/src/components/8.Result/ResultPage.js
+++ frontend/src/components/8.Result/ResultPage.js
@@ -5,6 +5,8 @@ import '../Common/Help.css';
 import MapModal from '../Common/MapModal';
 import Icon from '../Common/MapsPinpoint';
 
+const mapSrc = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3192.588180029826!2d174.7669186152492!3d-36.85233777993783!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6d0d47e383f32253%3A0xbd49f61f758a9e5b!2sThe%20University%20of%20Auckland!5e0!3m2!1sen!2snz!4v1615862553109!5m2!1sen!2snz';
+
 /**
  *
  * @return {*}
@@ -28,7 +30,7 @@ function ResultPage() {
           View on Google Maps
           <Icon />
         </button>
-        <MapModal trigger={MapPopup} setTrigger={setMapPopup}/>
+        <MapModal trigger={MapPopup} setTrigger={setMapPopup} mapSrc={mapSrc}/>
       </div>
 
       <button
