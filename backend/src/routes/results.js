195b86f811dfc7fc6b3a0fd20f4754c0f36bf851
Cecilia Yin <41213946+cecilia-yin@users.noreply.github.com>
2021-03-16 20:45:03 +1300

    Add post createResult endpoint

--- backend/src/routes/results.js
+++ backend/src/routes/results.js
@@ -1,6 +1,20 @@
 import express from 'express';
+import Result from '../mongo/modelsResult';
 const router = express.Router();
 
-// TODO: Handlers...
+// Post: Creating a result document in the result collection
+router.post('/', async (req, res) => {
+  const result = new Result({
+    sessionId: req.body.sessionId, result: req.body.result,
+  });
+
+  try {
+    const newResult = await result.save();
+    res.status(201).json(newResult);
+  } catch (error) {
+    res.status(400).json({message: error.message});
+  }
+});
 
 export default router;
+
