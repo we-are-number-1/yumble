75593ff641c33346ec6b803e37f0ed816c19512b
jemimahmartinez <martinezjemimah@gmail.com>
2021-03-16 16:28:09 +1300

    Created a button for viewing Google Maps

---
+++ frontend/src/components/Common/MapModal.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Help.css';
+
+/**
+ *
+ * @param {*} props
+ * @return {*}
+ */
+function MapModal(props) {
+  return props.trigger ? (
+    <div className='Help'>
+      <div className='Help-inner'>
+        <button
+          className='SmallBtn'
+          id='CloseButton'
+          onClick={() => props.setTrigger(false)}
+        >
+          close
+        </button>
+        {props.children}
+      </div>
+    </div>
+  ) : (
+    ''
+  );
+}
+
+export default MapModal;
