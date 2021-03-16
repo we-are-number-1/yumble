4fd7a5f473aac79458fa752f236eb1ab4cd40549
Yuxin Chen <yche864@aucklanduni.ac.nz>
2021-03-16 10:46:22 +0800

    Add Restaurant modelnames

--- backend/src/mongo/models/Restaurant.js
+++ backend/src/mongo/models/Restaurant.js
@@ -1,1 +1,12 @@
-// TODO: Restaurant Model
+import mongoose from 'mongoose';
+
+export const restaurantSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  numberOfVotes: {
+    type: Number,
+    default: 0,
+  },
+});
