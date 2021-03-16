f7bed78648f5e049d5625e4f30d60a9f18acfb10
jemimahmartinez <martinezjemimah@gmail.com>
2021-03-16 20:07:14 +1300

    Added an embedded placeholder map

--- frontend/src/components/8.Result/ResultPage.js
+++ frontend/src/components/8.Result/ResultPage.js
@@ -12,23 +12,21 @@ import Icon from '../Common/MapsPinpoint';
 function ResultPage() {
   const [ButtonPopup, setButtonPopup] = useState(false);
   const [MapPopup, setMapPopup] = useState(false);
+
   return (
     <>
       <div className='MakeCentre'>
-        <h1 className='Title'>Top Choice</h1>
+        <h1 className='ResultTitle'>Top Choice</h1>
+        <button
+          onClick={() => setMapPopup(true)}
+          className='BigBtn'
+          id='GoogleMaps_btn'
+        >
+          View on Google Maps
+          <Icon />
+        </button>
+        <MapModal trigger={MapPopup} setTrigger={setMapPopup}/>
       </div>
-
-      <button
-        onClick={() => setMapPopup(true)}
-        className='BigBtn'
-        id='GoogleMaps_btn'
-      >
-        View on Google Maps
-        <Icon />
-      </button>
-      <MapModal trigger={MapPopup} setTrigger={setMapPopup}>
-        {/* <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3192.588180029826!2d174.7669186152492!3d-36.85233777993783!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6d0d47e383f32253%3A0xbd49f61f758a9e5b!2sThe%20University%20of%20Auckland!5e0!3m2!1sen!2snz!4v1615862553109!5m2!1sen!2snz" width="600" height="450" style="border:0;" allowFullScreen="" loading="lazy"></iframe> */}
-      </MapModal>
 
       <button
         onClick={() => setButtonPopup(true)}
@@ -36,7 +34,8 @@ function ResultPage() {
         id='HelpButton'
       >help?</button>
       <Help trigger={ButtonPopup} setTrigger={setButtonPopup}>
-        <p>You are all set! Click on [View on Google Maps] to book.</p>
+        <p>You are all set! Click on [View on Google Maps]
+          to see where you and your friends are going.</p>
       </Help>
     </>
   );
--- frontend/src/components/Common/MapModal.js
+++ frontend/src/components/Common/MapModal.js
@@ -1,16 +1,6 @@
 import React from 'react';
 import './Help.css';
 
-const iFrame = () => {
-  const ifrm = document.createElement('iframe');
-  ifrm.setAttribute('src', 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3192.588180029826!2d174.7669186152492!3d-36.85233777993783!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6d0d47e383f32253%3A0xbd49f61f758a9e5b!2sThe%20University%20of%20Auckland!5e0!3m2!1sen!2snz!4v1615862553109!5m2!1sen!2snz');
-  ifrm.style.width = '600';
-  ifrm.style.height = '450';
-  ifrm.style.border = '0';
-  ifrm.allowFullscreen='';
-  ifrm.loading = 'lazy';
-};
-
 /**
  *
  * @param {*} props
@@ -20,13 +10,18 @@ function MapModal(props) {
   return props.trigger ? (
     <div className='Help'>
       <div className='Help-inner'>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3192.588180029826!2d174.7669186152492!3d-36.85233777993783!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6d0d47e383f32253%3A0xbd49f61f758a9e5b!2sThe%20University%20of%20Auckland!5e0!3m2!1sen!2snz!4v1615862553109!5m2!1sen!2snz"
+          width="600"
+          height="450"
+          border="0"
+          allowFullScreen=""
+          loading="lazy"
+        />
         <button
           className='SmallBtn'
           id='CloseButton'
-          onClick={() => {
-            props.setTrigger(false);
-            iFrame();
-          }}
+          onClick={() => props.setTrigger(false)}
         >
           close
         </button>
