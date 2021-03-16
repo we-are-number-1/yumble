0a8ed33488843c75f6c4a987aa4d59e17f37ec27
Tianrens <tshe695@aucklanduni.ac.nz>
2021-03-16 19:49:16 +1300

    Added new removeGames function in Games.js, modified disconnect.js to remove games once there are no more users

--- backend/src/sockets/disconnect.js
+++ backend/src/sockets/disconnect.js
@@ -1,9 +1,18 @@
+import games from '../domain/Games';
 /**
  * Socket event for when a user disconnects.
  * @param {*} socket
  */
 export function disconnect(socket) {
   socket.on('disconnect', () => {
+    const socketid = socket.id;
+    const activeGames = games.getGames();
+    activeGames.forEach((gameid, game) => {
+      game.session.users.filter((userid) => userid != socketid);
+      if (game.session.users.size == 0) {
+        games.removeGame(gameid);
+      }
+    });
     console.log('user disconnected with id:', socket.id);
   });
 }
