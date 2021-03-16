5d3b73fc57d7a5b066e123a38faeac45e87052ad
dagea <55040968+dagea@users.noreply.github.com>
2021-03-17 04:23:48 +1300

    Added preference setting related websockets to frontend and backend

--- backend/src/sockets/setPreferences.js
+++ backend/src/sockets/setPreferences.js
@@ -4,7 +4,12 @@
  * @param {*} io
  */
 export function updatePreference(socket, io) {
+<<<<<<< HEAD
   socket.on('set_preferences', ({sessionId, preferences}) => {
     io.to(sessionId).emit('set_preferences', preferences);
+=======
+  socket.on('set_preferences', ({sessionID, preferences}) => {
+    io.to(sessionID).emit('set_preferences', preferences);
+>>>>>>> Added preference setting related websockets to frontend and backend
   });
 }
--- frontend/src/sockets/setPreferences.js
+++ frontend/src/sockets/setPreferences.js
@@ -5,7 +5,11 @@
  */
  export function setPreferences(socket, cb) {
     socket.on('set_preferences', (preferences) => {
+<<<<<<< HEAD
       // Call back function with object containing updated preferences
+=======
+      // Call back function with next round data.
+>>>>>>> Added preference setting related websockets to frontend and backend
       cb(preferences);
     });
   }
