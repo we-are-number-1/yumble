51d3bcc7ef960b50360ee73c8a4beecff581b3d5
bche536 <bche536@aucklanduni.ac.nz>
2021-03-17 10:25:07 +1300

    added autocomplete searchbox, need to fix API key hiding

---
+++ frontend/src/components/4.Preferences/AutocompleteSearchBox.js
@@ -0,0 +1,65 @@
+import React, {useState} from 'react';
+
+import PlacesAutocomplete, {
+  geocodeByAddress,
+  getLatLng,
+} from 'react-places-autocomplete';
+
+const AutocompleteSearchBox = () => {
+  const [address, setAddress] = useState('');
+  const [coordinates, setCoordinates] = useState({lat: null, lng: null});
+
+  const handleSelect = async (value) => {
+    const results = await geocodeByAddress(value);
+    const latLng = await getLatLng(results[0]);
+    setAddress(value);
+    setCoordinates(latLng);
+  };
+
+  const google = window.google;
+
+  const searchOptions = {
+    location: new google.maps.LatLng(-36.8, 174.8),
+    radius: 2000,
+  };
+
+  return (
+    <div>
+      <PlacesAutocomplete
+        value={address}
+        onChange={setAddress}
+        onSelect={handleSelect}
+        searchOptions={searchOptions}
+      >
+        {({getInputProps, suggestions, getSuggestionItemProps, loading}) => (
+          <div>
+            <p>Latitude: {coordinates.lat}</p>
+            <p>Longitude: {coordinates.lng}</p>
+
+            <input {...getInputProps({placeholder: 'Enter a place'})} />
+            <div>
+              {loading ? <div>...loading</div> : null}
+
+              {suggestions.map((suggestion, index) => {
+                const style = {
+                  backgroundColor: suggestion.active ? '#41b6e6' : '#fff',
+                };
+
+                return (
+                  <div
+                    {...getSuggestionItemProps(suggestion, {style})}
+                    key={index}
+                  >
+                    {suggestion.description}
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        )}
+      </PlacesAutocomplete>
+    </div>
+  );
+};
+
+export default AutocompleteSearchBox;
