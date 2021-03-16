3ad8b6989e0c9a7e2223b26adea8e83d21789af9
Tianrens <tshe695@aucklanduni.ac.nz>
2021-03-16 19:34:07 +1300

    Fixed lint issues

--- frontend/src/sockets/gameStart.js
+++ frontend/src/sockets/gameStart.js
@@ -1,6 +1,7 @@
 /**
  * Emits socket event to indicate start of a game.
  * @param {*} socket
+ * @param {*} gameid
  */
 export function gameStart(socket, gameid) {
   socket.emit('game_start', ({gameid}));
