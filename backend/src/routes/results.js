8492ddb67ce966f0b751d29eda173f542d5b589e
Yuxin Chen <yche864@aucklanduni.ac.nz>
2021-03-16 18:57:03 +0800

    Solve conflict

--- backend/src/routes/results.js
+++ backend/src/routes/results.js
@@ -2,21 +2,6 @@ import express from 'express';
 import Result from '../mongo/models/Result';
 const router = express.Router();
 
-// Post: Creating a result object in the result collection
-router.post('/', async (req, res) => {
-  const result = new Result({
-    sessionId: req.body.sessionId,
-    restaurant: req.body.restaurant,
-  });
-
-  try {
-    const newResult = await result.save();
-    res.status(201).json(newResult);
-  } catch (error) {
-    res.status(400).json({message: error.message});
-  }
-});
-
 // GET: Get results
 router.get('/results/:id', async (req, res) => {
   const result = new Result({
