163222c0b4e2103f3e93870d02276f686d110d6f
dagea <55040968+dagea@users.noreply.github.com>
2021-03-17 04:29:29 +1300

    updated incorrect wording

--- backend/src/sockets/setPreferences.js
+++ backend/src/sockets/setPreferences.js
@@ -4,12 +4,7 @@
  * @param {*} io
  */
 export function updatePreference(socket, io) {
-<<<<<<< HEAD
   socket.on('set_preferences', ({sessionId, preferences}) => {
     io.to(sessionId).emit('set_preferences', preferences);
-=======
-  socket.on('set_preferences', ({sessionID, preferences}) => {
-    io.to(sessionID).emit('set_preferences', preferences);
->>>>>>> Added preference setting related websockets to frontend and backend
   });
 }
