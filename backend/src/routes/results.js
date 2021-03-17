69c6972c6bcc0cca0d6b0e73b50ef58559c42dc7
Yuxin Chen <yche864@aucklanduni.ac.nz>
2021-03-17 12:14:45 +0800

    fix bug

--- backend/src/routes/results.js
+++ backend/src/routes/results.js
@@ -2,7 +2,7 @@ import express from 'express';
 import Result from '../mongo/models/Result';
 const router = express.Router();
 
-// GET: Get results
+// GET: Get results of top three restaurants
 router.get('/results/:id', async (req, res) => {
   const result = new Result({
     sessionId: req.params.id,
