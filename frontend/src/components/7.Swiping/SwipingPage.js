2a8c711f96bfcd9f6331d6416c653f0e6d37c3c7
Ayush <ajani49@gmail.com>
2021-03-18 01:07:01 +1300

    Apply styling to card details and prepare for API calls

--- frontend/src/components/7.Swiping/SwipingPage.js
+++ frontend/src/components/7.Swiping/SwipingPage.js
@@ -9,6 +9,15 @@ import '../Common/Help.css';
  * @return {*}
  */
 function SwipingPage() {
+  const [ButtonPopup, setButtonPopup] = useState(false);
+  const SwipedRight= [];
+
+  let name = 'Lonestar';
+  let location = 'Botany';
+  let cuisine = 'European';
+  let price = '$$$';
+  let rating = '4.0';
+
   useEffect(() => {
     document.title = 'Yes or No?';
   }, []);
@@ -18,21 +27,64 @@ function SwipingPage() {
       console.log(response.data);
     });
   };
+  
+  /**
+ * @param {null} Adds the current restaurant details to array which stores
+ * all right-swiped restaurants.
+ * @return {void}
+ */
+  function clickedYes() {
+    console.log('clicked yes');
+    const restaurant = {name, location, cuisine, price, rating};
+    SwipedRight.push(restaurant);
+    console.log(SwipedRight);
+    getNewCard();
+  }
+
+  /**
+ * @param {null} Retrieves new restaurant details
+ * @return {void}
+ */
+  function getNewCard() {
+    name = 'Burger King';
+    location = 'Manukau';
+    cuisine = 'Cheap shit';
+    price = '$';
+    rating = '3.0';
+  }
+
+  /**
+ * @param {null} Retrieves new restaurant details
+ * @return {void}
+ */
+  function cardClicked() {
+    console.log('Card clicked');
+  }
+  
 
   return (
     <>
       <h1 className='Title'> yumble</h1>
       <h1 className='TimeCounter'> Remaining time: 21s </h1>
       <div className='MakeCentre'>
-        <button className='YesOrNoButton' id='YesButton'>Keen!</button>
+        <button
+          className='YesOrNoButton'
+          id='YesButton'
+          onClick = {clickedYes}>
+            Keen!
+        </button>
         <button className='YesOrNoButton' id='NoButton'>Nope!</button>
-        <div className="SwipeCard">
+        <div className="SwipeCard" onClick = {cardClicked}>
           <div className="CardImage"></div>
-          </div>
-          {/* <button className='SmallBtn' id='DetailsButton'>
-            View more detail
-          </button> */}
+          <p className='PlaceDetails' id = 'name'> {name} </p>
+          <p className='PlaceDetails' id= 'location'> {location} </p>
+          <p className='PlaceDetails' id = 'price'> {price} </p>
+          <p className='PlaceDetails' id = 'cuisine'> {cuisine} </p>
+          <p className='PlaceDetails' id = 'rating'> {rating} Rating</p>
         </div>
+        {/* <button className='SmallBtn' id='DetailsButton'>
+          View more detail
+        </button> */}
         <button className='BigBtn'>View on google maps</button>
       </div>
       <button
@@ -43,13 +95,13 @@ function SwipingPage() {
         help?
       </button>
       <Help trigger={ButtonPopup} setTrigger={setButtonPopup}>
-        <text>
+        <p>
           - Press yes if you are keen to potentially visit this restaurant
           otherwise press no.
           <br></br>
           - View this restaurant on the map by clicking the green button
           below the card.
-        </text>
+        </p>
       </Help>
     </>
   );
