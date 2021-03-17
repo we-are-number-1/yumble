2653f4351cbf9921612f815a51858e40b7e5a23d
Cecilia Yin <41213946+cecilia-yin@users.noreply.github.com>
2021-03-17 21:35:12 +1300

    [FEAT] Add Post createResult implementation to backend  (#93)

--- backend/src/routes/results.js
+++ backend/src/routes/results.js
@@ -1,6 +1,20 @@
 import express from 'express';
+import Result from '../mongo/models/Result';
 const router = express.Router();
 
-// TODO: Handlers...
+// Post: Creating a result object in the result collection
+router.post('/', async (req, res) => {
+  const result = new Result({
+    sessionID: req.body.sessionID, restaurants: req.body.restaurants,
+  });
+
+  try {
+    const newResult = await result.save();
+    res.status(201).json(newResult);
+  } catch (error) {
+    res.status(400).json({message: error.message});
+  }
+});
 
 export default router;
+
