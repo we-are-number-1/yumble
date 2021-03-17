f57b19a3e09c3580616d2ddc3aab0c49ae110dbb
Yuxin Chen <yche864@aucklanduni.ac.nz>
2021-03-17 17:16:59 +0800

    fix bug

--- backend/src/routes/results.js
+++ backend/src/routes/results.js
@@ -4,14 +4,17 @@ const router = express.Router();
 
 // GET: Get results of top three restaurants
 router.get('/:id', async (req, res) => {
-  Result.find({sessionId: req.params.id}, function(err, docs) {
-    if (error) {
-      console.log(err);
-    } else {
-      res.status(200).json(docs);
+  let result;
+  try {
+    result = await Result.find({sessionId: req.params.id});
+    if (result == null) {
+      return res.status(404).json({message: 'Can not find the result'});
     }
-  });
+  } catch (error) {
+    res.status(500).json({message: error.message});
+  }
 });
+
 // Post: Creating a result object in the result collection
 router.post('/', async (req, res) => {
   const result = new Result({
