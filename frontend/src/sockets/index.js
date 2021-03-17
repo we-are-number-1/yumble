1e0434e06546fb5e37b2e6ce8a84fadad0d50a17
dagea <55040968+dagea@users.noreply.github.com>
2021-03-17 18:11:07 +1300

    Resolved merge conflict

--- frontend/src/sockets/index.js
+++ frontend/src/sockets/index.js
@@ -4,8 +4,4 @@ export {joinRoom} from './joinRoom';
 export {endGame} from './endGame';
 export {start} from './start';
 export {nextRound} from './nextRound';
-<<<<<<< HEAD
 export {countdown} from './countdown';
-=======
-export {countdown} from './countdown'
->>>>>>> 601cee8ad1a15b258b541d450be9011ea385f0e7
