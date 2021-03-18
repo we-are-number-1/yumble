c5d61704878c4c5419d52f4a5f15befd68a8baa5
Ben Chen <55371154+bche536@users.noreply.github.com>
2021-03-18 23:33:46 +1300

    refactored handleSelect function and made a new Common file (#140)
    happy with the reviews

--- frontend/src/components/4.Preferences/AutocompleteSearchBox.js
+++ frontend/src/components/4.Preferences/AutocompleteSearchBox.js
@@ -1,19 +1,15 @@
 import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
-import PlacesAutocomplete, {
-  geocodeByAddress,
-  getLatLng,
-} from 'react-places-autocomplete';
+import PlacesAutocomplete from 'react-places-autocomplete';
+import {getLocationCoordinates} from '../Common/LocationHelper';
 
 const AutocompleteSearchBox = () => {
   const [address, setAddress] = useState('');
   const [coordinates, setCoordinates] = useState({lat: null, lng: null});
 
   const handleSelect = async (value) => {
-    const results = await geocodeByAddress(value);
-    const latLng = await getLatLng(results[0]);
     setAddress(value);
-    setCoordinates(latLng);
+    setCoordinates(getLocationCoordinates(value));
   };
 
   const google = window.google;
