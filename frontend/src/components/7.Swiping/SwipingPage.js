cec7bcb5e866ff13f17013fa70e1ef7a53795300
Ayush <ajani49@gmail.com>
2021-03-18 23:51:02 +1300

    Implement Map modal button

--- frontend/src/components/7.Swiping/SwipingPage.js
+++ frontend/src/components/7.Swiping/SwipingPage.js
@@ -1,7 +1,13 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
+
 import Help from '../Common/Help';
 import '../Common/Help.css';
+import MapModal from '../Common/MapModal';
+import Icon from '../Common/MapsPinpoint';
+
+const mapSrc = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3192.588180029826!2d174.7669186152492!3d-36.85233777993783!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6d0d47e383f32253%3A0xbd49f61f758a9e5b!2sThe%20University%20of%20Auckland!5e0!3m2!1sen!2snz!4v1615862553109!5m2!1sen!2snz';
+
 
 /**
  *
@@ -9,6 +15,7 @@ import '../Common/Help.css';
  */
 function SwipingPage() {
   const [ButtonPopup, setButtonPopup] = useState(false);
+  const [MapPopup, setMapPopup] = useState(false);
   const SwipedRight= [];
 
   const RemainingTime = '25';
@@ -68,10 +75,16 @@ function SwipingPage() {
         <button
           className='YesOrNoButton'
           id='YesButton'
-          onClick = {clickedYes}>
+          onClick = {clickedYes}
+        >
             Keen!
         </button>
-        <button className='YesOrNoButton' id='NoButton'>Nope!</button>
+        <button
+          className='YesOrNoButton'
+          id='NoButton'
+        >
+            Nope!
+        </button>
         <div className="SwipeCard" onClick = {cardClicked}>
           <div className="CardImage"></div>
           <p className='PlaceDetails' id = 'name'> {name} </p>
@@ -80,10 +93,15 @@ function SwipingPage() {
           <p className='PlaceDetails' id = 'cuisine'> {cuisine} </p>
           <p className='PlaceDetails' id = 'rating'> {rating} Rating</p>
         </div>
-        {/* <button className='SmallBtn' id='DetailsButton'>
-          View more detail
-        </button> */}
-        <button className='BigBtn'>View on google maps</button>
+        <button
+          onClick={() => setMapPopup(true)}
+          className='BigBtn'
+          id='GoogleMaps_btn'
+        >
+          View on Google Maps
+          <Icon />
+        </button>
+        <MapModal trigger={MapPopup} setTrigger={setMapPopup} mapSrc={mapSrc}/>
       </div>
       <button
         onClick={() => setButtonPopup(true)}
