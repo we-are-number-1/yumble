3d15be5cf633abc03df729c82f85ff5e0f75b088
Tianrens <tshe695@aucklanduni.ac.nz>
2021-03-18 21:58:32 +1300

    Added test for new game and test for tests being persisted

---
+++ backend/src/domain/__tests__/Games.test.js
@@ -0,0 +1,12 @@
+import games from '../Games';
+import {SocketSession} from '../models/SocketSession';
+
+test('add game', () => {
+  const session = new SocketSession('1234', null, {roundInterval: 3});
+  games.newGame(null, session, null);
+  expect(games.getGame('1234')).toBeDefined();
+});
+
+test('game is persisted', () => {
+  expect(games.getGame('1234')).toBeDefined();
+});
