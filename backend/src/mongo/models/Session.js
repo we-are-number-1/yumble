ac6700866eaf8d3d8c8c04d29dc3dc4192d25472
BlinkCreator <49140391+BlinkCreator@users.noreply.github.com>
2021-03-19 20:40:17 +1300

    [Yumble]:PE-141 Modify api to follow new contract

--- backend/src/mongo/models/Session.js
+++ backend/src/mongo/models/Session.js
@@ -1,11 +1,25 @@
 import mongoose from 'mongoose';
 
+import {preferenceSchema} from './Preference';
+import {restaurantSchema} from './Restaurant';
+
 const sessionSchema = mongoose.Schema({
+  truncCode: {
+    type: String,
+    default: '',
+  },
   isFinished: {
     type: Boolean,
     default: false,
   },
+  preferences: {
+    type: preferenceSchema,
+    required: true,
+  },
+  results: {
+    type: [restaurantSchema],
+    required: true,
+  },
 });
 
 export default mongoose.model('Session', sessionSchema);
-
