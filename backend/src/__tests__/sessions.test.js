e0c0b438c72c216914209e8b48d5588c1565c50d
Aniket Rai <arai759@aucklanduni.ac.nz>
2021-03-19 16:43:37 +1300

    Updating tests to match test data

--- backend/src/__tests__/sessions.test.js
+++ backend/src/__tests__/sessions.test.js
@@ -10,9 +10,9 @@ describe('GET /sessions/testCard', () => {
   test('It should respond with a basic static JSON file', async () => {
     const response = await request(app).get('/sessions/testCard');
     expect(response.body.name).toEqual('Lonestar');
-    expect(response.body.location).toEqual('Owen G Glen Building');
+    expect(response.body.location).toEqual('Wiri');
     expect(response.body.cuisine).toEqual(['European']);
-    expect(response.body.price).toEqual([5]);
+    expect(response.body.price).toEqual('$$');
     expect(response.statusCode).toBe(200); ;
   });
 });
