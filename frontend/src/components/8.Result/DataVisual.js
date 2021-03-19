7e3fb76309fe6db0777c8432b70bcc169429b1e9
jemimahmartinez <martinezjemimah@gmail.com>
2021-03-19 18:46:55 +1300

    Created a data visual. Still need to style

---
+++ frontend/src/components/8.Result/DataVisual.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Pie} from 'react-chartjs-2';
+
+/**
+ * @param {*} props
+ * @return {*}
+ */
+function DataVisual(props) {
+  return (
+    <div className='PieChart'>
+      <Pie data={props.data} />
+    </div>
+  );
+}
+
+export default DataVisual;
--- frontend/src/components/8.Result/ResultPage.js
+++ frontend/src/components/8.Result/ResultPage.js
@@ -3,6 +3,36 @@ import Help from '../Common/Help';
 import '../Common/Help.css';
 import MapModal from '../Common/MapModal';
 import Icon from '../Common/MapsPinpoint';
+import DataVisual from './DataVisual';
+
+const data = {
+  // Replace labels with restuarant names from 'swiping' pages
+  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+  datasets: [
+    {
+      label: '# of Votes',
+      // Replace numbers with number of votes from 'swiping' page
+      data: [12, 19, 3, 5, 2, 3],
+      borderColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+      ],
+      backgroundColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
 
 /**
  *
@@ -19,6 +49,10 @@ function ResultPage() {
     <>
       <div className='MakeCentre'>
         <h1 className='ResultTitle'>Top Choice</h1>
+        <div className='MainContainer'>
+          <h1 className='ResultTitle'>*CARD*</h1>
+          <DataVisual data={data}/>
+        </div>
         <button
           onClick={() => setMapPopup(true)}
           className='BigBtn'
