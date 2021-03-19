7d6885a96a0266743dc7555738c42f5bd4b820eb
David Xiao <davidjinxiao@hotmail.com>
2021-03-19 13:50:36 +1300

    created inital test and mocks

---
+++ backend/src/domain/__mocks__/Games.js
@@ -0,0 +1,30 @@
+import {SocketSession} from '../models/SocketSession';
+
+/**
+  * This class stores a list of all active games in session
+  */
+export class Games {
+  /**
+    * Initiates map storing all game sessions
+    */
+  constructor() {
+    this.activeGames = new Map();
+  }
+
+  /**
+    *
+    * @param {*} sessionId
+    * @return {*} game for give id
+    */
+  getGame(sessionId) {
+    return {
+      session: {
+        sessionId: sessionId,
+        addUser: function( socket, name) {},
+      },
+      startCountdown: function() { },
+    };
+  }
+}
+
+export default new Games();
