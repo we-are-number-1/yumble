a14846617af52fed557d93bac89e8bc5822be0e1
Ayush Jani <61725957+ayushjani@users.noreply.github.com>
2021-03-21 23:26:06 +1300

    [FEAT] Fix the styling issues for cards (#214)

--- frontend/src/components/Common/SwipeCard.js
+++ frontend/src/components/Common/SwipeCard.js
@@ -17,15 +17,21 @@ function cardClicked() {
 function SwipeCard(props) {
   const data = props.data;
   const name = data.name;
+  const images = data.images;
   const location = data.location;
   const cuisine = data.cuisine;
   const price = data.price;
   const rating = data.rating;
+  console.log(images);
 
   return (
     <div className="SwipeCard" onClick = {cardClicked}>
-      <div className="CardImage"></div>
-      <p className='PlaceDetails' id = 'name'> {name} </p>
+      <div className="CardImage">
+        <img className="CardImage" src={images} ></img>
+      </div>
+      <div>
+        <p className='PlaceDetails' id = 'name'> {name} </p>
+      </div>
       <p className='PlaceDetails' id= 'location'> {location} </p>
       <p className='PlaceDetails' id = 'price'> {price} </p>
       <p className='PlaceDetails' id = 'cuisine'> {cuisine} </p>
