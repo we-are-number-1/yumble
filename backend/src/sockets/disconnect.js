e2db249ae613c9b1c57e06e618b34e57a53297b4
Tianrens <tshe695@aucklanduni.ac.nz>
2021-03-24 17:12:18 +1300

    Removed commented out code

--- backend/src/sockets/disconnect.js
+++ backend/src/sockets/disconnect.js
@@ -18,17 +18,6 @@ export function disconnect(socket, io, cb) {
         io.to(game.session.sessionId).emit('new_user', {users: users});
       }
     });
-    // let sessionId = null;
-    // activeGames.forEach((game) => {
-    //   game?.session?.users?.filter((userId) => userId != socketId);
-    //   if (game?.session?.users?.size == 0) {
-    //     sessionId = game.session.sessionId;
-    //   }
-    // });
-    // if (sessionId) {
-    //   games.removeGame(sessionId);
-    // }
-
     console.log('user disconnected with id:', socket.id);
     cb();
   });
