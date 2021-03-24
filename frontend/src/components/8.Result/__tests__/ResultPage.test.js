78f2c4903db7dae6016491d8a0398c636f2c034d
jemimahmartinez <martinezjemimah@gmail.com>
2021-03-24 22:33:55 +1300

    Updated ResultPage test

--- frontend/src/components/8.Result/__tests__/ResultPage.test.js
+++ frontend/src/components/8.Result/__tests__/ResultPage.test.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import {BrowserRouter} from 'react-router-dom';
 
-import Result from '../ResultPage';
+import ResultPage from '../ResultPage';
 
 test('ResultPage snapshot testing', () => {
-  jest.mock('../../Common/SwipeCard', () => ({
-    SwipeCard: 'SwipeCard',
-  }));
+  // jest.mock('../../Common/SwipeCard', () => ({
+  //   SwipeCard: 'SwipeCard',
+  // }));
 
-  jest.mock('../../Map/Map', () => ({
-    Map: 'Map',
-  }));
+  // jest.mock('../../Map/Map', () => ({
+  //   Map: 'Map',
+  // }));
 
-  const tree = renderer.create(<Result />).toJSON();
+  const tree = renderer.create(
+      <BrowserRouter>
+        <ResultPage />
+      </BrowserRouter>).toJSON();
   expect(tree).toMatchSnapshot();
 });
