2b80fbd9ba1a2eae8c5df522621ee8a11120318b
jemimahmartinez <martinezjemimah@gmail.com>
2021-03-24 18:05:09 +1300

    Fixed obsolete snapshot test

--- frontend/src/components/8.Result/__tests__/MapModal.test.js
+++ frontend/src/components/8.Result/__tests__/MapModal.test.js
@@ -3,50 +3,41 @@ import renderer from 'react-test-renderer';
 
 import MapModal from '../../Common/MapModal';
 
-test('MapModal snapshot testing without defining the trigger', () => {
-  jest.mock('../../Map/Map', () => ({
-    Map: 'Map',
-  }));
-
-  const tree = renderer
-      .create(
-          <MapModal
-            retaurantLocation={{lat: -36.8523, lng: 174.7691}}
-          />,
-      )
-      .toJSON();
-  expect(tree).toMatchSnapshot();
-});
-
-test('MapModal snapshot testing with defining a trigger', () => {
-  //   const [MapPopup, setMapPopup] = useState(false);
-
+test('MapModal snapshot testing with/without defining the trigger', () => {
   // Cache original functionality
-  const realUseState = React.useState;
+  const mockSetMapPopup = React.useState;
 
   // Stub the initial state
-  const stubInitialState = 'stub data';
+  const MapPopup = 'true';
 
   // Mock useState before rendering your component
   jest
       .spyOn(React, 'useState')
-      .mockImplementationOnce(() => realUseState(stubInitialState));
+      .mockImplementationOnce(() => mockSetMapPopup(false));
 
   jest.mock('../../Map/Map', () => ({
     Map: 'Map',
   }));
 
-  console.log({stubInitialState});
-  const tree = renderer
+  // Testing without defining the trigger
+  const tree1 = renderer
       .create(
           <MapModal
-          // trigger={MapPopup}
-          // setTrigger={setMapPopup}
-            trigger={stubInitialState}
-            setTrigger={stubInitialState}
             retaurantLocation={{lat: -36.8523, lng: 174.7691}}
           />,
       )
       .toJSON();
-  expect(tree).toMatchSnapshot();
+  expect(tree1).toMatchSnapshot();
+
+  // Testing with defining the trigger
+  const tree2 = renderer
+      .create(
+          <MapModal
+            trigger={MapPopup}
+            setTrigger={mockSetMapPopup}
+            retaurantLocation={{lat: -36.8523, lng: 174.7691}}
+          />,
+      )
+      .toJSON();
+  expect(tree2).toMatchSnapshot();
 });
