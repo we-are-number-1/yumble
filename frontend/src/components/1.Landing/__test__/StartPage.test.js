11ef56d46a9795486669a17b30fb92fd498572e3
Wong <wcho400@aucklanduni.ac.nz>
2021-03-24 20:55:30 +1300

    Fix linting

--- frontend/src/components/1.Landing/__test__/StartPage.test.js
+++ frontend/src/components/1.Landing/__test__/StartPage.test.js
@@ -1,13 +1,13 @@
-import React from "react";
-import renderer from "react-test-renderer";
-import StartPage from "../StartPage";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import renderer from 'react-test-renderer';
+import StartPage from '../StartPage';
+import {BrowserRouter} from 'react-router-dom';
 
-test("Start Page snapshot testing", () => {
+test('Start Page snapshot testing', () => {
   const component = renderer.create(
-    <BrowserRouter>
-      <StartPage />
-    </BrowserRouter>
+      <BrowserRouter>
+        <StartPage />
+      </BrowserRouter>,
   );
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
