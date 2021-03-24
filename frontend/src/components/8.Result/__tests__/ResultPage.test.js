9f542170fef96f90c9427c1a061c06c6eecee8d4
jemimahmartinez <martinezjemimah@gmail.com>
2021-03-24 18:30:06 +1300

    Mocked SwipeCard for ResultPage test

--- frontend/src/components/8.Result/__tests__/ResultPage.test.js
+++ frontend/src/components/8.Result/__tests__/ResultPage.test.js
@@ -4,6 +4,9 @@ import renderer from 'react-test-renderer';
 import Result from '../ResultPage';
 
 test('ResultPage snapshot testing', () => {
+  jest.mock('../../Common/SwipeCard', () => ({
+    SwipeCard: 'SwipeCard',
+  }));
   const tree = renderer.create(<Result />).toJSON();
   expect(tree).toMatchSnapshot();
 });
