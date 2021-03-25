63e265e9a23ba013d3a6ee9623278ecc35b6b962
Wong <wcho400@aucklanduni.ac.nz>
2021-03-25 21:12:54 +1300

    Snapshot for Help Component

--- frontend/src/components/Common/__tests__/MapsPinpoint.test.js
+++ frontend/src/components/Common/__tests__/MapsPinpoint.test.js
@@ -2,14 +2,13 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import MapPinpoint from '../MapsPinpoint';
 
-it('Maps Pinpoint snapshot testing', () => {
-  const component1 = renderer.create(
+test('Maps Pinpoint snapshot testing', () => {
+  const component = renderer.create(
       <>
         <MapPinpoint />
       </>,
   );
 
-  const tree1 = component1.toJSON();
-  expect(tree1).toMatchSnapshot();
-
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
 });
