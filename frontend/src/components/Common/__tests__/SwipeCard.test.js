b69236e80c63fcee1c342ba6d1bed836770d7f10
Ayush Jani <61725957+ayushjani@users.noreply.github.com>
2021-03-25 22:20:30 +1300

    [TEST] Swiping page testing (#232)
    * Add a test for the swipe-card
    
    * Minor clean-up changes
    
    * Change location of common swipe card test
    
    * Add test for swiping page
    
    * REDO: Add test for swiping page
    
    * fix conflicts
    
    * update swiping page snapshot
    
    Co-authored-by: Ayush <ajani49@gmail.com>

---
+++ frontend/src/components/Common/__tests__/SwipeCard.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import SwipeCard from '../SwipeCard';
+
+test('Swiping Card snapshot testing', () => {
+  const name = 'Lonestar';
+  const images= 'https://c.files.bbci.co.uk/050B/production/_103119210_lazytown2.jpg';
+  const location = 'Botany';
+  const cuisine = 'European';
+  const price = '$$$';
+  const rating = '4.0';
+  const Data = {name, images, location, cuisine, price, rating};
+
+  const tree = renderer.create(<SwipeCard data = {Data}/>).toJSON();
+  expect(tree).toMatchSnapshot();
+});
