725d6006cd8a77ecf7eed8009c57d6afdabbeaeb
Anmol Thapar <68182660+absternator@users.noreply.github.com>
2021-04-10 17:11:00 +1200

    Feature/landing join mobile responsiveness and css (#283)
    * 'first 2 pages css update'
    
    * startpage fix
    
    * join game screen
    
    * fixing join group
    
    * 'button fix'
    
    * empty
    
    * added new line to end of file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import StartPage from './components/1.Landing/StartPage';
 import JoinGroup from './components/2.Join/JoinGroup';
 import Preferences from './components/3.Preferences/Preferences';
 import Lobby from './components/4.Lobby/Lobby';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Countdown from './components/5.Countdown/CountDown';
 import Swiping from './components/6.Swiping/SwipingPage';
 import Result from './components/7.Result/ResultPage';
