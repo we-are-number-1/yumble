bed7f84a2c79bfc1b9e1088f664b9586673f5f8a
dhors <smexylure@gmail.com>
2021-04-11 02:07:37 +1200

    fixed the map modal css, and also made it resizeable.

--- frontend/src/components/Common/MapModal.js
+++ frontend/src/components/Common/MapModal.js
@@ -1,24 +1,29 @@
 import React from 'react';
 import './Help.css';
 import Map from '../Map/Map';
+import Alert from 'react-bootstrap/Alert';
+import Button from 'react-bootstrap/Button';
 
 /**
  *
  * @param {*} props
  * @return {*}
- */
+ */ 
 function MapModal(props) {
   return props.trigger ? (
     <div className='Help'>
       <div className='Help-inner'>
-        <Map restaurantLocations={props.restaurantLocation} />
-        <button
-          className='SmallBtn'
-          id='CloseButton'
-          onClick={() => props.setTrigger(false)}
-        >
-          close
-        </button>
+        <Alert variant='info' className="overflow-hidden">
+          <Alert.Heading className="text-center">Restaurant Location</Alert.Heading>
+          <p><Map restaurantLocations={props.restaurantLocation} /></p>
+          <Button
+            variant='outline-danger'
+            onClick={() => props.setTrigger(false)}
+          >
+            close!
+          </Button>
+          <hr />
+        </Alert>
       </div>
     </div>
   ) : (
