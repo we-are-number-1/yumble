b168991c99343b712cd4c2e9adf9a2f17d9ae591
Reeve <dcunhareeve@gmail.com>
2021-04-20 20:13:47 +1200

    change svg to another one

--- frontend/src/components/Common/MapsPinpoint.js
+++ frontend/src/components/Common/MapsPinpoint.js
@@ -3,7 +3,7 @@ import React from 'react';
 /**
  *
  * @return {*}
- * 
+ *
  * This is the location icon.
  * Used in the swiping page and result page to show a location icon
  * on the respective buttons.
@@ -11,22 +11,13 @@ import React from 'react';
 function Icon() {
   return (
     <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="-90 -90 600 600"
-      width="30"
-      height="30"
+      xmlns='http://www.w3.org/2000/svg'
+      width='30'
+      height='30'
+      fill='#ff6056'
+      viewBox='0 0 16 16'
     >
-      <path
-        fill="#ff6056"
-        width="100%"
-        height="100%"
-        // TODO
-        // eslint-disable-next-line max-len
-        d="M256 0C161.896 0 85.333 76.563 85.333 170.667c0 28.25 7.063 56.26 20.49 
-          81.104L246.667 506.5c1.875 3.396 5.448 5.5 9.333 5.5s7.458-2.104 
-          9.333-5.5l140.896-254.813c13.375-24.76 20.438-52.771 20.438-81.021C426.667
-          76.563 350.104 0 256 0zm0 256c-47.052 0-85.333-38.281-85.333-85.333S208.948 
-          85.334 256 85.334s85.333 38.281 85.333 85.333S303.052 256 256 256z"></path>
+      <path d='M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z' />
     </svg>
   );
 }
