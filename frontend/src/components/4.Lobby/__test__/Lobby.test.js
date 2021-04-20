595e5ce70366eb76326bfc8060c020f2d10fdd24
Reeve <dcunhareeve@gmail.com>
2021-04-20 19:31:11 +1200

    Additional snapshot and jest fixes

--- frontend/src/components/4.Lobby/__test__/Lobby.test.js
+++ frontend/src/components/4.Lobby/__test__/Lobby.test.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import Lobby from '../Lobby';
-import {SocketContextProvider} from '../../../sockets/SocketContext';
+import { SocketContextProvider } from '../../../sockets/SocketContext';
+import { BrowserRouter } from 'react-router-dom';
 
 jest.mock('./../../../sockets/SocketContext');
 jest.mock('./../../../sockets');
 
 test('Lobby snapshot testing', () => {
-  const tree = renderer.create(
-      <SocketContextProvider>
-        <Lobby location={location}/>
-      </SocketContextProvider>).toJSON();
+  const tree = renderer
+    .create(
+      <BrowserRouter>
+        <SocketContextProvider>
+          <Lobby location={location} />
+        </SocketContextProvider>
+      </BrowserRouter>
+    )
+    .toJSON();
   expect(tree).toMatchSnapshot();
 });
--- frontend/src/components/5.Countdown/__test__/CountDown.test.js
+++ frontend/src/components/5.Countdown/__test__/CountDown.test.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import CountDown from '../CountDown';
-import {SocketContextProvider} from '../../../sockets/SocketContext';
+import { SocketContextProvider } from '../../../sockets/SocketContext';
 
 jest.mock('./../../../sockets/SocketContext');
 jest.mock('./../../../sockets');
 
 test('CountDown snapshot testing', () => {
-  const tree = renderer.create(<SocketContextProvider>
-    <CountDown location={location}/>
-  </SocketContextProvider>).toJSON();
+  const tree = renderer
+    .create(
+      <SocketContextProvider>
+        <CountDown location={location} />
+      </SocketContextProvider>
+    )
+    .toJSON();
   expect(tree).toMatchSnapshot();
 });
